b911ef603085e3c0f50befc4b244e22742a73717
Dan Pascu <dan@ag-projects.com>
2020-02-07 17:07:44 +0000

    Refactored setup.py for PEP-8 compliance

--- setup.py
+++ setup.py
@@ -6,38 +6,39 @@ import glob
 
 import sipclient
 
+setup(
+    name='sipclients',
+    version=sipclient.__version__,
 
-setup(name         = "sipclients",
-      version      = sipclient.__version__,
-      author       = "AG Projects",
-      author_email = "support@ag-projects.com",
-      url          = "http://sipsimpleclient.org",
-      description  = "SIP SIMPLE client",
-      long_description = "Python command line clients using the SIP SIMPLE framework",
-      platforms    = ["Platform Independent"],
-      classifiers  = [
-          "Development Status :: 4 - Beta",
-          #"Development Status :: 5 - Production/Stable",
-          #"Development Status :: 6 - Mature",
-          "Intended Audience :: Service Providers",
-          "License :: GNU Lesser General Public License (LGPL)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python"
-      ],
-      packages   = ["sipclient", "sipclient.configuration"],
-      data_files = [('share/sipclients/sounds', glob.glob(os.path.join('resources', 'sounds', '*.wav')))],
-      scripts = ["sip-audio-session",
-                 "sip-message",
-                 "sip-publish-presence",
-                 "sip-register",
-                 "sip-session",
-                 "sip-settings",
-                 "sip-subscribe-mwi",
-                 "sip-subscribe-presence",
-                 "sip-subscribe-rls",
-                 "sip-subscribe-winfo",
-                 "sip-subscribe-xcap-diff"
-                 ]
+    description='SIP SIMPLE client',
+    long_description='Python command line clients using the SIP SIMPLE framework',
+    url='http://sipsimpleclient.org',
+
+    author='AG Projects',
+    author_email='support@ag-projects.com',
+
+    platforms=['Platform Independent'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Service Providers',
+        'License :: GNU Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+
+    packages=['sipclient', 'sipclient.configuration'],
+    data_files=[('share/sipclients/sounds', glob.glob(os.path.join('resources', 'sounds', '*.wav')))],
+    scripts=[
+        'sip-audio-session',
+        'sip-message',
+        'sip-publish-presence',
+        'sip-register',
+        'sip-session',
+        'sip-settings',
+        'sip-subscribe-mwi',
+        'sip-subscribe-presence',
+        'sip-subscribe-rls',
+        'sip-subscribe-winfo',
+        'sip-subscribe-xcap-diff'
+    ]
 )
-
-
